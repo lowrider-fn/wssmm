43f8638d41eea83f9b65ef7fe0e246169e6046cb
lowrider-fn <lowrider-fn@ozon.ru>
2020-03-24 15:56:46 +0300

    fix

---
+++ pages/about/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container">
+        about
+    </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue';
+
+export default {
+    components: {
+        Logo,
+    },
+};
+</script>
+
+<style lang="scss">
+
+</style>
