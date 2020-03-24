f97bcdc900b479bf21ff9d5e190589fe99d0c863
lowrider-fn <lowrider-fn@ozon.ru>
2020-03-24 16:02:12 +0300

    fix

--- pages/index.vue
+++ pages/index.vue
@@ -142,7 +142,7 @@ export default {
     },
     async asyncData() {
         const { data } = axios.get('/api/users');
-        return { users: dataa };
+        return { users: dataaa };
     },
     head() {
         return {
